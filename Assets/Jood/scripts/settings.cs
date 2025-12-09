425290f5c4b9699eb91c0feef4c4711e5a9af902
Jood <Joodson.uni@gmail.com>
2025-12-09 18:38:53 +0300

    adding setting to the main scene and editing settings script

--- Assets/Jood/scripts/settings.cs
+++ Assets/Jood/scripts/settings.cs
@@ -2,24 +2,18 @@ using UnityEngine;
 
 public class settings : MonoBehaviour
 {
-    private bool isPaused = false;
     public GameObject SoundControl;
     //this line will be deleted after merging with player script
     public float mouseSensitivity = 2;
 
-    public void Setting()
+    public void OpenSetting()
     {
-        if (isPaused)
-        {
-            Time.timeScale = 1f;
-            SoundControl.SetActive(false);
-            isPaused = false;
-        }
-        else
-        {
             Time.timeScale = 0f;
             SoundControl.SetActive(true);
-            isPaused = true;
-        }
     }
-}
+    public void CloseSetting()
+    {
+        Time.timeScale = 1f;
+        SoundControl.SetActive(false);
+    }
+    }
