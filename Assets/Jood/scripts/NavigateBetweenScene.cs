60e4f7149d7170abea78d17bc2b698f7dbe16260
Jood <Joodson.uni@gmail.com>
2025-12-09 17:58:50 +0300

    change navigate between scenes script

--- Assets/Jood/scripts/NavigateBetweenScene.cs
+++ Assets/Jood/scripts/NavigateBetweenScene.cs
@@ -3,8 +3,26 @@ using UnityEngine.SceneManagement;
 
 public class NavigateBetweenScene : MonoBehaviour
 {
+    private static string lastScene = null;
+
     public void LoadSceneByName(string sceneName)
     {
+        // Save current scene as "last scene"
+        lastScene = SceneManager.GetActiveScene().name;
+
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void LoadLastScene()
+    {
+        if (!string.IsNullOrEmpty(lastScene))
+        {
+            SceneManager.LoadScene(lastScene);
+        }
+        else
+        {
+            // fallback if no history
+            SceneManager.LoadScene("Jood");  
+        }
     }
 }
